5a8c6bee3101c4ddf5317d5179933b158244e16b
shoski <sho25052007@gmail.com>
2023-09-28 15:18:55 +0100

    feat: initial prismic set up

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,58 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'paraprint-prismic',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "paraprint-prismic",
+        htmlAttrs: {
+            lang: "en",
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            { hid: "description", name: "description", content: "" },
+            { name: "format-detection", content: "telephone=no" },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/tailwindcss
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/prismic",
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    prismic: {
+        endpoint: "https://paraprint-prismic.cdn.prismic.io/api/v2",
+        modern: true,
+        apiOptions: {
+            routes: [
+                // Resolves the Homepage document to "/"
+                {
+                    type: "homepage",
+                    path: "/",
+                },
+            ],
+        },
+    },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial/>
+    <!-- <h1>{{ document.data.slices[0].primary.heading[0].text }}</h1> -->
+    <SliceZone :components="components" :slices="document.data.slices" />
 </template>
 
 <script>
+import { components } from "~/slices"
+
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    data: function () {
+        return { components }
+    },
+    async asyncData({ $prismic, error }) {
+        const document = await $prismic.api.getByUID("homepage", "home")
+
+        if (document) {
+            console.log(document.data)
+            return { document }
+        } else {
+            error({ statusCode: 404, message: "Page not found" })
+        }
+    },
 }
 </script>
